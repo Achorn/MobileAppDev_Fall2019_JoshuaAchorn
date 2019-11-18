09452de43849f136dc638438caa1c665909cce07
achorn <joshuaachorn@gmail.com>
2019-11-18 12:52:38 -0700

    added ratio calculator

--- project02/coffeehelper/app/src/main/java/com/example/coffeehelper/MainActivity.java
+++ project02/coffeehelper/app/src/main/java/com/example/coffeehelper/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTvTime;
     private Button mButtonStart;
     private Button mButtonStop;
-//    private Button mButtonReset;
 
 
+    //for calculator
+    private EditText cofText;
+    private Button calc;
+    private TextView ansCof;
+    private TextView ansWa;
+    private Switch cups;
+
+    //for timer
     private Context mContext;
     private Chronometer mChromometer;
     private Thread mThreadChrono;
@@ -27,12 +37,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContext = this;
+
+        //ratio calc
+        cofText = findViewById(R.id.etCoffee);
+        calc = findViewById(R.id.btnCalculate);
+        ansCof = findViewById(R.id.tvCoffee);
+        ansWa = findViewById(R.id.tvWater);
+        cups = findViewById(R.id.cupsSwitch);
+
+//        cups.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked == true)
+//            }
+//        });
+
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int coffeeVal = Integer.parseInt(cofText.getText().toString());
+                int waterVal = coffeeVal*16;
+
+                if(cups.isChecked()){
+                    ansCof.setText(coffeeVal*2 +"g");
+                    ansWa.setText(waterVal*2 + "g");
+                }
+                else {
+                    ansCof.setText(coffeeVal + "g");
+                    ansWa.setText(waterVal + "g");
+                }
+            }
+        });
+
+
+
+
+
+
 
         mTvTime = findViewById(R.id.tv_time);
         mButtonStart = findViewById(R.id.btnStart);
         mButtonStop = findViewById(R.id.btnStop);
-//        mButtonReset = findViewById(R.id.btnReset);
+
+        mContext = this;
 
         mButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
